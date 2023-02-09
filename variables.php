f996697ba9eec48aead4bd3ce482033808608d10
Brany Flores Gil <100183837+branyflores@users.noreply.github.com>
2023-02-09 18:32:28 +1100

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,8 @@
+<?php
+$firstName = "James";
+$accountBalance = 123.36;
+$allowEntry = false;
+$students = array("none", "james ", "helen", "aidan", "briony", "brany");
+echo $students[5];
+
+echo count($students);
